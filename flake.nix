80ac2604144c3d09b998162446ec6dc2bed022d0
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-09-01 15:47:10 +1000

    refactor(flake): refactored nix flake based on recommendations

--- flake.nix
+++ flake.nix
@@ -3,43 +3,61 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    dynamixel = {
+      url = "github:/ROBOTIS-GIT/DynamixelSDK";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self
+    , nixpkgs
+    , dynamixel
+    ,
+    }:
     let
       pkgs = import nixpkgs { system = "aarch64-linux"; };
-      dynamixel-sdk = pkgs.stdenv.mkDerivation rec {
+      dynamixel-sdk = pkgs.stdenv.mkDerivation {
         name = "dynamixel-sdk";
-        src = pkgs.fetchFromGitHub {
-          owner = "ROBOTIS-GIT";
-          repo = "DynamixelSDK";
-          rev = "2ded684dff05a40ac78d6a16105c6ddc1b3b9930";
-          sha256 = "sha256-NJynAxKJH/VPyYTWbS+56yTKOmXyo5B7pXwiN2gnbXY=";
-        };
-        nativeBuildInputs = with pkgs; [ cmake gcc ninja ];
-        
+        version = "0.0.0";
+        src = dynamixel;
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+        ];
+
         # Override all phases to do our own thing
-        phases = "installPhase";
-        
-        installPhase = ''
-          mkdir -p $out
-          # Create build dir in writable location
-          mkdir -p /tmp/dxl-build
-          cd /tmp/dxl-build
-          cmake $src/c++ -DCMAKE_INSTALL_PREFIX=$out
-          cmake --build .
-          cmake --install .
-        '';
+        # phases = "installPhase";
+
+        # installPhase = ''
+        #   mkdir -p $out
+        #   # Create build dir in writable location
+        #   mkdir -p /tmp/dxl-build
+        #   cd /tmp/dxl-build
+        #   cmake $src/c++ -DCMAKE_INSTALL_PREFIX=$out
+        #   cmake --build .
+        #   cmake --install .
+        # '';
       };
-    in {
+    in
+    {
       devShells.aarch64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [ gcc cmake ninja dynamixel-sdk ];
+        packages = with pkgs; [
+          gcc
+          cmake
+          ninja
+          dynamixel-sdk
+        ];
       };
 
       packages.aarch64-linux.default = pkgs.stdenv.mkDerivation {
         name = "dynamixel-controller";
         src = self;
-        nativeBuildInputs = with pkgs; [ cmake gcc ninja ];
+        nativeBuildInputs = with pkgs; [
+          cmake
+          gcc
+          ninja
+        ];
         buildInputs = [ dynamixel-sdk ];
         cmakeFlags = [
           "-DCMAKE_PREFIX_PATH=${dynamixel-sdk}"
