46347df3ce991308189ab1f57c76ef6580252903
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-09-12 12:30:27 +1000

    feat(nix flake): added support for x86 systems

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     dynamixel = {
       url = "github:/ROBOTIS-GIT/DynamixelSDK";
       flake = false;
@@ -12,70 +13,72 @@
   outputs =
     { self
     , nixpkgs
+    , flake-utils
     , dynamixel
     ,
     }:
-    let
-      pkgs = import nixpkgs { system = "aarch64-linux"; };
-      dynamixel-sdk = pkgs.stdenv.mkDerivation {
-        name = "dynamixel-sdk";
-        version = "0.0.0";
-        src = "${dynamixel}";
-        nativeBuildInputs = with pkgs; [
-          cmake
-          ninja
-        ];
+    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        dynamixel-sdk = pkgs.stdenv.mkDerivation {
+          name = "dynamixel-sdk";
+          version = "0.0.0";
+          src = "${dynamixel}";
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+          ];
 
-        configurePhase = ''
-          mkdir -p /tmp/dxl-build
-          cd /tmp/dxl-build
-          cmake $src/c++ -DCMAKE_INSTALL_PREFIX=$out
-        '';
+          configurePhase = ''
+            mkdir -p /tmp/dxl-build
+            cd /tmp/dxl-build
+            cmake $src/c++ -DCMAKE_INSTALL_PREFIX=$out
+          '';
 
-        buildPhase = ''
-          cd /tmp/dxl-build
-          cmake --build .
-        '';
+          buildPhase = ''
+            cd /tmp/dxl-build
+            cmake --build .
+          '';
 
-        installPhase = ''
-          mkdir -p $out
-          cd /tmp/dxl-build
-          cmake --install .
-        '';
-      };
-    in
-    {
-      devShells.aarch64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          gcc
-          cmake
-          ninja
-          dynamixel-sdk
-        ];
-      };
+          installPhase = ''
+            mkdir -p $out
+            cd /tmp/dxl-build
+            cmake --install .
+          '';
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            cmake
+            ninja
+            dynamixel-sdk
+          ];
+        };
 
-      packages.aarch64-linux.default = pkgs.stdenv.mkDerivation {
-        name = "dynamixel-controller";
-        src = self;
-        nativeBuildInputs = with pkgs; [
-          cmake
-          gcc
-          ninja
-        ];
-        buildInputs = [ dynamixel-sdk ];
-        cmakeFlags = [
-          "-DCMAKE_PREFIX_PATH=${dynamixel-sdk}"
-          "-DCMAKE_BUILD_TYPE=Release"
-        ];
-        phases = "installPhase";
-        installPhase = ''
-          mkdir -p $out/bin
-          mkdir -p /tmp/build
-          cd /tmp/build
-          cmake $src -DCMAKE_PREFIX_PATH=${dynamixel-sdk} -DCMAKE_BUILD_TYPE=Release
-          cmake --build .
-          cp dynamixal-controller $out/bin/
-        '';
-      };
-    };
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "dynamixel-controller";
+          src = self;
+          nativeBuildInputs = with pkgs; [
+            cmake
+            gcc
+            ninja
+          ];
+          buildInputs = [ dynamixel-sdk ];
+          cmakeFlags = [
+            "-DCMAKE_PREFIX_PATH=${dynamixel-sdk}"
+            "-DCMAKE_BUILD_TYPE=Release"
+          ];
+          phases = "installPhase";
+          installPhase = ''
+            mkdir -p $out/bin
+            mkdir -p /tmp/build
+            cd /tmp/build
+            cmake $src -DCMAKE_PREFIX_PATH=${dynamixel-sdk} -DCMAKE_BUILD_TYPE=Release
+            cmake --build .
+            cp dynamixal-controller $out/bin/
+          '';
+        };
+      });
 }
